36109283986a35a293b387e03ea3fd3afb50cfea
64kbytes <64kgames@gmail.com>
2017-04-21 14:25:40 -0300

    Base package code

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Baytree\KnowItFirst\KnowItFirstServiceProvider::class
+
     ],
 
     /*
---
+++ packages/baytree/knowitfirst/src/KnowItFirstServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Baytree\KnowItFirst;
+
+use Illuminate\Support\ServiceProvider;
+
+class KnowItFirstServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
